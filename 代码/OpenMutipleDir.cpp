301be6516ceb13f5a85209f14e16826a3ffbaa2b
cse0001 <1336195654@qq.com>
2021-06-02 13:01:49 +0800

    绝对路径访问目录和文件

--- 代码/OpenMutipleDir.cpp
+++ 代码/OpenMutipleDir.cpp
@@ -5,8 +5,22 @@ void OpenMutipleDir(string route)
     vector<string> directory;
     directory = split(route, "/");
     int n = directory.size();
-    for (int i = 0; i < n; i++)
+    if (strcmp(directory[0].c_str(), "root") == 0)
     {
-        OpenDir(directory[i].c_str());
+        while (dir_pointer > 1)
+        {
+            OpenDir("..");
+        }
+        for (int i = 1; i < n; i++)
+        {
+            OpenDir(directory[i].c_str());
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            OpenDir(directory[i].c_str());
+        }
     }
 }
